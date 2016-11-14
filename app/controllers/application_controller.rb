b0cc26bacbb0f0de297c46f26d8f9d5c28bf7c54
Andrew Gladilin <andrewgladilin@gmail.com>
2016-11-14 21:46:36 +0300

    Add locales support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def index
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
