74d670090c6009ee875e3ba442e4f74181ecf250
Andrew Gladilin <andrewgladilin@gmail.com>
2017-01-11 19:30:56 +0300

    Improving header navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
   root :to => 'posts#index'
-  get '/about' => 'application#index'
+  get '/about' => 'application#index', as: :about
   get '/achievements' => 'application#achievements'
 end
