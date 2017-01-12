3cd714d4886773c4f32dac77b2324174a61a8a45
Andrew Gladilin <andrewgladilin@gmail.com>
2017-01-13 01:39:38 +0300

    Sign up page done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   root :to => 'posts#index'
+
+  devise_scope :user do
+    get "/sign_up" => "devise/registrations#new", as: :sign_up
+    get "/sign_in" => "devise/sessions#new", as: :sign_in
+    get "/sign_out" => "devise/sessions#destroy", as: :sign_out
+    get "/settings" => "devise/registrations#edit", as: :settings
+  end
+  
   get '/about' => 'application#index', as: :about
   get '/achievements' => 'application#achievements'
 end
