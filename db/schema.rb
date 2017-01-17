ebcbc92f0670e01f8cf83a26d25467bde020601e
Andrew Gladilin <andrewgladilin@gmail.com>
2017-01-17 20:18:52 +0300

    Add summary field for Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111204024) do
+ActiveRecord::Schema.define(version: 20170117164711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170111204024) do
     t.string   "text",       default: "", null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.text     "summary",    default: ""
   end
 
   create_table "users", force: :cascade do |t|
