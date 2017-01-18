19aec1bffb58fb4ac3123a27dca9c6b18ebcb382
Andrew Gladilin <andrewgladilin@gmail.com>
2017-01-18 17:57:34 +0300

    Add views field and icon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117171906) do
+ActiveRecord::Schema.define(version: 20170118145102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170117171906) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.text     "summary",    default: ""
+    t.integer  "views",      default: 0
   end
 
   create_table "tags", force: :cascade do |t|
