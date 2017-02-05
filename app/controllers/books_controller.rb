cd22606db005e1208cf7d3b1423d57a10f6c96cf
Andrew Gladilin <andrewgladilin@gmail.com>
2017-02-05 15:13:02 +0300

    Add summary field to views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:name, :pages_read, :total_pages, :read, :book_picture, :link, :review)
+      params.require(:book).permit(:name, :pages_read, :total_pages, :read, :book_picture, :link, :review, :summary)
     end
 end
