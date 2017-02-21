61b4d9535b8a3cf8636d051a6954f8698d02c84a
Andrew Gladilin <andrewgladilin@gmail.com>
2017-02-21 21:48:19 +0300

    Add comments better styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,23 +2,10 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:update, :destroy]
 
   def create
-    # @comment = Comment.new(comment_params)
+    # I just don't know how to do that better.
     @resource = eval("comment_params[:resource_type].chop.capitalize.classify.constantize.find(comment_params[:resource_id])")
     @resource.comments.create(user_id: comment_params[:user_id], message: comment_params[:message], answer: comment_params[:answer])
-    # case comment_params[:resource_type]
-    #   'books': Book.create
-
-    # if @comment.save
-    #   redirect_to @comment, notice: 'Comment was successfully created.'
-    # respond_to do |format|
-    #   if @comment.save
-    #     format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-    #     format.json { render :show, status: :created, location: @comment }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @comment.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    redirect_to @resource
   end
 
   def update
