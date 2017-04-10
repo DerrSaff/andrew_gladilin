da1618aa821259a799a27d2abdcc583de8935532
Andrew Gladilin <andrewgladilin@gmail.com>
2017-04-10 23:14:15 +0300

    [84] add content to the links section

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   get '/about' => 'application#index', as: :about
-  get '/links' => 'application#links', as: :links
+  get '/bookmarks' => 'application#bookmarks', as: :bookmarks
+  get '/databases' => 'application#databases', as: :databases
+  get '/languages' => 'application#languages', as: :languages
+  get '/books' => 'application#books', as: :books
   root to: 'application#index'
 end
