db7bb8eddc3ac50bd7ef1205c04b88704e13002a
Andrew <andrewgladilin@gmail.com>
2017-11-04 23:29:04 +0300

    Kinda initial commit

--- client/main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import { Meteor } from 'meteor/meteor';
-
-import App from './../imports/ui/App'
-
-Meteor.startup(() => {
-  ReactDOM.render(<App />, document.getElementById('app'))
-});
